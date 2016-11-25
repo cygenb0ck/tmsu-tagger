be3b8b812c9ac148c043c1152865dd2a45ef6486
max <mh@stealth.at>
2016-11-25 16:29:28 +0100

    first version of tagging works

--- tagger.py
+++ tagger.py
@@ -4,37 +4,88 @@
 import os
 import subprocess
 import taglib
+import re
 
 """
 
-- check if tmsu init was called - if .tmsu folder exists
-- tmsu init if needed
 - for all files in tree: extract metadata, tag files
 
 
 """
 
 data_dir = "./test_data"
+mount_point_name = "mp"
+full_mount_point = data_dir + "/" + mount_point_name
 
 
-def init_tmsu(tmsu_dir):
-    if not os.path.isdir(tmsu_dir + "/.tmsu") or not os.path.exists(tmsu_dir + "/.tmsu/db"):
-        p = subprocess.Popen(["tmsu", "init"], cwd=tmsu_dir)
+def init_tmsu(tmsu_root, mount_point):
+    """
+    initializes tmsu at tmsu_root and creates mount_point for usage as tmsu mountpoint
+    :param tmsu_root: path to tmsu_root, ex. "./data"
+    :param mount_point: path to mp inside tmsu_root, ex. "./data/mp"
+    :return:
+    """
+    print("----->" + tmsu_root)
+    if not os.path.isdir(tmsu_root + "/.tmsu") or not os.path.exists(tmsu_root + "/.tmsu/db"):
+        p = subprocess.Popen(["tmsu", "init"], cwd=tmsu_root)
         p.wait()
 
+    if not os.path.exists( mount_point):
+        os.mkdir(mount_point)
+
+
+def escape_shell_chars(str):
+    """
+    escapes all chars which would cause problems in shell
+    :param str:
+    :return:
+    """
+    return re.sub("(!|\$|#|&|\"|\'|\(|\)|\||<|>|`|\\\|;)", r"\\\1", str)
+
+
+def tag(filename_rel_to_tmsu_root, tmsu_root):
+    """
+
+    :param filename_rel_to_tmsu_root:
+    :param tmsu_root:
+    :return:
+    """
+    print("####################")
+    print(tmsu_root, filename_rel_to_tmsu_root)
+    full_filename = tmsu_root + "/" +  filename_rel_to_tmsu_root
+    print(full_filename)
+
+    try:
+        tl_file = taglib.File(full_filename)
+        tags = []
+        for tag_name, tag_value in tl_file.tags.items():
+            for s in tag_value:
+                tags.append( "{0}={1}".format(escape_shell_chars(tag_name.lower()), escape_shell_chars(s)))
+        cmd = ["tmsu", "tag", filename_rel_to_tmsu_root] + tags
+        print( cmd)
+        p = subprocess.Popen(cmd, cwd=tmsu_root)
+        p.wait()
+
+    except OSError as e:
+        pass
 
 
 if __name__ == "__main__":
-    print("hello world")
+    exclude_dirs = [full_mount_point, data_dir + "/.tmsu"]
+    print(exclude_dirs)
 
     for dirName, subDirList, fileList in os.walk(data_dir):
+        print("dn", dirName)
+        if dirName in exclude_dirs:
+            print("skipping ", dirName)
+            continue
         if len(fileList) > 0:
             for f in fileList:
-                print("{0}/{1}".format(dirName, f))
-                try:
-                    s = taglib.File( dirName + "/" + f )
-                    #print(s.tags)
-                    for k, v in s.tags.items():
-                        print("  ", k, v)
-                except OSError as e:
-                    pass
+                print("\t{0}/{1}".format(dirName, f))
+                # try:
+                #     s = taglib.File( dirName + "/" + f )
+                #     #print(s.tags)
+                #     for k, v in s.tags.items():
+                #         print("  ", k, v)
+                # except OSError as e:
+                #     pass
--- tests.py
+++ tests.py
@@ -4,10 +4,18 @@
 import unittest
 import os
 import shutil
+import subprocess
 
 import tagger
 
 data_dir = "./test_data"
+
+tmsu_dir = data_dir + "/.tmsu"
+mount_point = "mp"
+mount_point_full_path = data_dir + "/" + mount_point
+
+music_file = "Bibi Blocksberg - Hex Hex (Remix).ogg"
+
 
 class TestTMSUInit(unittest.TestCase):
     def setUp(self):
@@ -16,15 +24,67 @@ class TestTMSUInit(unittest.TestCase):
 
     def tearDown(self):
         self._remove_tmsu()
+        shutil.rmtree(mount_point_full_path)
+        pass
 
-    def _remove_tmsu(self):
-        if os.path.exists(self.tmsu_dir):
-            shutil.rmtree(self.tmsu_dir)
+    @staticmethod
+    def _remove_tmsu():
+        if os.path.exists(tmsu_dir):
+            shutil.rmtree(tmsu_dir)
 
     def test_init(self):
-        tagger.init_tmsu(data_dir)
-        self.assertTrue(os.path.isdir(self.tmsu_dir))
-        self.assertTrue(os.path.exists(self.tmsu_dir+"/db"))
+        tagger.init_tmsu(data_dir, mount_point_full_path)
+
+        self.assertTrue(os.path.isdir(tmsu_dir))
+        self.assertTrue(os.path.exists(tmsu_dir+"/db"))
+        self.assertTrue(os.path.isdir(mount_point_full_path))
+
+
+# class TestTagging(unittest.TestCase):
+#     def setUp(self):
+#         print("2222222222")
+#         tagger.init_tmsu(data_dir, mount_point_full_path)
+#         self.mount()
+#
+#     def tearDown(self):
+#         self.unmount()
+#         # dont remove, because it's mounted
+#         # TestTMSUInit._remove_tmsu()
+#
+#     @staticmethod
+#     def mount():
+#         print("MOUNTING")
+#         p = subprocess.Popen(["tmsu", "mount", mount_point ], cwd=data_dir)
+#         p.wait()
+#         pass
+#
+#     @staticmethod
+#     def unmount():
+#         # wait until tmsu can unmount on it's own on ubuntu 14.04
+#         pass
+#
+#     def test_tagging(self):
+#         check_values = {
+#             'artist': ['Bibi Blocksberg'],
+#             'compilation': ['0'],
+#             'date': ['-1'],
+#             'title': ['Hex Hex (Remix)'],
+#             'tracknumber': ['-1']
+#         }
+#
+#         tagger.tag(music_file, data_dir)
+#
+#         print(mount_point_full_path, "/tags")
+#
+#         for dir_name, subdir_list, file_list in os.walk(mount_point_full_path + "/tags"):
+#             print(dir_name)
+#             for f in file_list:
+#                 print("\t", f)
+#                 self.assertTrue( f in check_values.keys() )
+
+
+
+
 
 
 if __name__ == "__main__":
